f85d65de6511e1e18af3c5cd30b86dd9934394bd
Jacob Su <suzp1984@gmail.com>
2021-03-11 16:03:14 +0800

    add directional light example

---
+++ DirectionalLight/DirectionalLight/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DirectionalLight
+//
+//  Created by Jacob Su on 3/11/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DirectionalLight/DirectionalLight/ShapeShaderType.m
@@ -0,0 +1,8 @@
+//
+//  ShapeShaderType.m
+//  DirectionalLight
+//
+//  Created by Jacob Su on 3/11/21.
+//
+
+#import <Foundation/Foundation.h>
